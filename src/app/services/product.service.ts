eeaa576c2bd257f8df2542d3819698b70b52b3b7
mypolaris218 <winter.polaris0214@gmail.com>
2025-04-18 03:25:47 -0700

    product service added

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Product } from '../models/product.model';
+import { Observable, of } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class ProductService {
+  private products: Product[] = [
+    { id: 1, name: 'Phone', price: 699, description: 'A smart phone' },
+    { id: 2, name: 'Laptop', price: 1299, description: 'A powerful laptop' }
+  ];
 
-  constructor() { }
+  getProducts(): Observable<Product[]> {
+    return of(this.products);
+  }
+
+  getProductById(id: number): Observable<Product | undefined> {
+    return of(this.products.find(p => p.id === id));
+  }
 }
